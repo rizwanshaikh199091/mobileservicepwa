9723cb46d601b847e789e651d3d477a87251cf1a
Rizwan shaikh <rizwanshaikh@Rizwans-MacBook-Pro.local>
2021-01-25 17:44:06 +0530

    new changes

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 //let cacheData = "appV1";
-var CACHE_NAME = 'appV1';
+/*var CACHE_NAME = 'appV1';
 var urlsToCache = [
     "./static/js/bundle.js",
     "./static/js/main.chunk.js",
@@ -24,4 +24,41 @@ this.addEventListener('fetch', function(event) {
             return response || fetch(event.request);
         })
     );
-});
+});*/
+
+var cacheName = 'js13kPWA-v1';
+var appShellFiles = [
+    "/static/js/bundle.js",
+    "/static/js/main.chunk.js",
+    "/static/js/0.chunk.js",
+    "/index.html",
+    "/"
+];
+
+var contentToCache = appShellFiles;
+
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+    e.waitUntil(
+      caches.open(cacheName).then((cache) => {
+            console.log('[Service Worker] Caching all: app shell and content');
+        return cache.addAll(contentToCache);
+      })
+    );
+  });
+
+
+  self.addEventListener('fetch', (e) => {
+    e.respondWith(
+      caches.match(e.request).then((r) => {
+            console.log('[Service Worker] Fetching resource: '+e.request.url);
+        return r || fetch(e.request).then((response) => {
+                  return caches.open(cacheName).then((cache) => {
+            console.log('[Service Worker] Caching new resource: '+e.request.url);
+            cache.put(e.request, response.clone());
+            return response;
+          });
+        });
+      })
+    );
+  });
