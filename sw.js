732b5a9564cd4a1ad3ca59150ec87d4bbced563c
Rizwan shaikh <rizwanshaikh@Rizwans-MacBook-Pro.local>
2021-01-25 16:52:02 +0530

    upadint pwa work

--- sw.js
+++ sw.js
@@ -1,26 +1,27 @@
-let cacheData = "appV1";
+//let cacheData = "appV1";
+var CACHE_NAME = 'appV1';
+var urlsToCache = [
+    "/static/js/bundle.js",
+    "/static/js/main.chunk.js",
+    "/static/js/0.chunk.js",
+    "/index.html",
+    "/"
+];
+this.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        // Open a cache and cache our files
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
 
-this.addEventListener("install", (event) =>{
-    event.waitUntil(
-        caches.open(cacheData).then((cache)=>{
-            cache.addAll([
-                "/static/js/bundle.js",
-                "/static/js/main.chunk.js",
-                "/static/js/0.chunk.js",
-                "/index.html",
-                "/"
-
-            ])
-        })
-    )
-})
-
-this.addEventListener("fetch", (event)=>{
+this.addEventListener('fetch', function(event) {
+    console.log(event.request.url);
     event.respondWith(
-        caches.match(event.request).then((result)=>{
-            if(result){
-                return result;
-            }
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
         })
-    )
-})
+    );
+});
