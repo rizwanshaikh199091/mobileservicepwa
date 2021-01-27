4f4d84674bc36a16d876c75d387da0d39fd3aa1a
Rizwan shaikh <rizwanshaikh@Rizwans-MacBook-Pro.local>
2021-01-27 16:48:51 +0530

    new chagnes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cacheName = 'js13kPWA-v1';
+/*var cacheName = 'js13kPWA-v1';
 var appShellFiles = [
     "./static/js/bundle.js",
     "./static/js/main.chunk.js",
@@ -8,6 +8,8 @@ var appShellFiles = [
 ];
 
 var contentToCache = appShellFiles;
+
+
 
 this.addEventListener('install', (e) => {
     console.log('[Service Worker] Install');
@@ -31,6 +33,65 @@ this.addEventListener('install', (e) => {
             return response;
           });
         });
+      })
+    );
+  });*/
+
+var cache_name = 'mspwa-cache';
+var cached_urls = [
+    "/static/js/bundle.js",
+    "/static/js/main.chunk.js",
+    "/static/js/0.chunk.js",
+    "/index.html",
+    "/",
+    "menifest.json",
+    "/logo144.png"
+];
+
+this.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cache_name)
+    .then(function(cache) {
+      return cache.addAll(cached_urls);
+    })
+  );
+});
+
+this.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName.startsWith('pages-cache-') && staticCacheName !== cacheName) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+this.addEventListener('fetch', function(event) {
+    console.log('Fetch event for ', event.request.url);
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        if (response) {
+          console.log('Found ', event.request.url, ' in cache');
+          return response;
+        }
+        console.log('Network request for ', event.request.url);
+        return fetch(event.request).then(function(response) {
+          if (response.status === 404) {
+            return caches.match('fourohfour.html');
+          }
+          return caches.open(cached_urls).then(function(cache) {
+           cache.put(event.request.url, response.clone());
+            return response;
+          });
+        });
+      }).catch(function(error) {
+        console.log('Error, ', error);
+        return caches.match('offline.html');
       })
     );
   });
