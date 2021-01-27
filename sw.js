9fc3170100d93c09fc9c207faaa45f8091166bf0
Rizwan shaikh <rizwanshaikh@Rizwans-MacBook-Pro.local>
2021-01-27 15:26:08 +0530

    new chan ges

--- sw.js
+++ sw.js
@@ -1,31 +1,3 @@
-//let cacheData = "appV1";
-/*var CACHE_NAME = 'appV1';
-var urlsToCache = [
-    "./static/js/bundle.js",
-    "./static/js/main.chunk.js",
-    "./static/js/0.chunk.js",
-    "./index.html",
-    "./"
-];
-this.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        // Open a cache and cache our files
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-this.addEventListener('fetch', function(event) {
-    console.log(event.request.url);
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
-    );
-});*/
-
 var cacheName = 'js13kPWA-v1';
 var appShellFiles = [
     "/static/js/bundle.js",
