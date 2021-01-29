382872278e8dee266d7efea439b6c7a7d84925ed
Rizwan shaikh <rizwanshaikh@Rizwans-MacBook-Pro.local>
2021-01-29 13:07:58 +0530

    new changes

--- sw.js
+++ sw.js
@@ -22,11 +22,11 @@ self.addEventListener('install', function(event) {
         // fichiers à mettre impérativement en cache
         return staticCache.addAll([
           "./",
-          "./static/js/2.cdb3059e.chunk.js",
-          "./static/css/2.31ff0196.chunk.css",
-          "./static/js/main.aa7dff3a.chunk.js",
-          "./static/js/runtime-main.5bc791e0.js",
-          "./static/css/main.155c1d57.chunk.css",
+          "./static/js/2.fb3894cd.chunk.js",
+          "./static/css/2.620f3d88.chunk.css",
+         "./static/js/main.3b4a716d.chunk.js",
+         "./static/js/runtime-main.5bc791e0.js",
+         "./static/css/main.155c1d57.chunk.css",
           "./index.html",
           "./logo144.png"
         ])
